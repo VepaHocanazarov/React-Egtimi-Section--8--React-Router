2ada962275b0e0c423aa972fa50fa46adc9d1bef
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-13 08:49:26 +0300

    67. Switch İle 404 Mesajı Verme (başarısız)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import "./App.css";
 import './index.css';
-import { Navigate } from "react-router-dom"
+import { Outlet }from "react-router-dom"
 import About from "./About";
 import News from "./News";
 import { BrowserRouter, Link, NavLink, Route, Routes, } from 'react-router-dom';
 
 const Profile = () => {
   return (<h1>Profile: Vepa Hocanazarov</h1>)
+};
+
+const Error = () => {
+  return (<h1>404 Not Found</h1>)
 }
 
 class App extends Component {
@@ -30,20 +34,24 @@ class App extends Component {
         <NavLink className="vepa" to="/News">News</NavLink>
         <br /><br />
         <NavLink className="vepa" to="/Profile">Profile</NavLink>
+        <br /><br />
+        <NavLink className="vepa" to="/Error">Error</NavLink>
 
 
         <br /><br />
 
         <input type="button" onClick={this.clickbutton} value={this.state.loggedin ? "Log Out" : "Login"} />
 
+           
+         
         <Routes>
           <Route path='/About' element={<About />} />
           <Route path='/News' element={<News />} />
-          <Route path='/Profile' exact strict render={() => (
-
-            this.state.loggedin ? (<Profile />) : (<Navigate to="/" />)
-          )} />
+          <Route path='/Profile' element={<Profile />} />
+          <Route path="/Error" element={<Error/>}/>
+          
         </Routes>
+        
       </div>
     );
   }
